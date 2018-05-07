280bc9121c84ea07879cd7f720546abe5368f405
nobita <congson1293@gmail.com>
2018-05-07 19:08:31 +0700

    decrease TRENDING_MERGE_THRESHOLD

--- master.py
+++ master.py
@@ -16,7 +16,7 @@ from sklearn.externals import joblib
 
 warnings.filterwarnings('ignore', category=UserWarning)
 
-TRENDING_MERGE_THRESHOLD = 0.35
+TRENDING_MERGE_THRESHOLD = 0.1
 
 class master:
     def __init__(self):
