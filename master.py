aec4eeb0be9e89c73a96cc7759b58f40f9a1494b
nobita <congson1293@gmail.com>
2018-05-20 19:44:37 +0700

    change HOUR_TO_RESET

--- master.py
+++ master.py
@@ -22,7 +22,7 @@ import regex
 warnings.filterwarnings('ignore', category=UserWarning)
 
 TRENDING_MERGE_THRESHOLD = 0.0
-HOUR_TO_RESET = 3  # reset at 3h AM
+HOUR_TO_RESET = 0  # reset at 3h AM
 
 class master:
     def __init__(self):
