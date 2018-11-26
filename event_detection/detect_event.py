ef6184854df6b6f8f795b548c2de241639a5e027
nobita <congson1293@gmail.com>
2018-11-26 16:38:49 +0700

    add comment

--- event_detection/detect_event.py
+++ event_detection/detect_event.py
@@ -12,7 +12,7 @@ from sklearn.cluster import DBSCAN
 MIN_DOCS = 5
 MIN_TRENDING_DOCS = 8
 MIN_SAMPLES = 4
-EPS = 0.6
+EPS = 0.6  # cosine distance
 
 class event_detection:
     def __init__(self, domain, dataset, root_dir='.'):
