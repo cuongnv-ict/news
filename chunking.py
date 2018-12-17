d685a848329cc7cdcc6fc7cdf7c939d1b8eff8ed
nobita <congson1293@gmail.com>
2018-12-17 17:36:13 +0700

    update chunking

--- chunking.py
+++ chunking.py
@@ -4,7 +4,7 @@ from nltk.tokenize import word_tokenize
 
 
 
-blacklist_POS = ['CC', 'DT', 'IN', 'TO', 'PRP']
+blacklist_POS = ['CC', 'DT', 'IN', 'TO', 'PRP', 'MD']
 
 
 def process_content(s):
@@ -15,22 +15,31 @@ def process_content(s):
         lemmatizer = WordNetLemmatizer()
         words = map(lambda w: lemmatizer.lemmatize(w), words)
 
-        # stem = PorterStemmer()
-        # words = map(lambda w: stem.stem(w), words)
-
         tagged = pos_tag(words)
-        chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?<DT>?<JJ>*<NN>}"""
+        chunkGram = r"""Chunk: {<RB.?>*<VB.?>*<NNP>+<NN>?}
+                               {<DT>?<JJ>*<NN>}"""
         chunkParser = RegexpParser(chunkGram)
         chunked = chunkParser.parse(tagged)
 
-        print(chunked)
-        for subtree in chunked.subtrees(filter=lambda t: t.label() == 'Chunk'):
-            print(subtree)
+        # print(chunked)
+        # for subtree in chunked.subtrees(filter=lambda t: t.label() == 'Chunk'):
+        #     print(subtree)
+        #
+        # chunked.draw()
 
-        chunked.draw()
+        stem = PorterStemmer()
 
         for subtree in chunked:
-            pass
+            if type(subtree) is type(chunked):
+                new_s.append(u'_'.join([stem.stem(x[0]) for x in subtree]))
+                continue
+            if subtree[1] in blacklist_POS:
+                continue
+            new_s.append(stem.stem(subtree[0]))
+
+        new_s = u' '.join(new_s)
+        return new_s
+
 
     except Exception as e:
         print(str(e))
@@ -38,5 +47,5 @@ def process_content(s):
 
 
 
-s = 'He liked to discuss about the issues.'
+s = 'British Prime Minister Theresa May will firmly reject a second Brexit referendum in an address to Parliament Monday, as pressure for a new vote grows both inside and outside a bitterly divided Westminster.'
 process_content(s)
